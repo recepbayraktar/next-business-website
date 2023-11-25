a3d8ea72f90ebbb89b4ce2422ff4a2783e9f3aca
Recep Bayraktar <recepbayraktar0@gmail.com>
2023-11-25 23:45:20 +0300

    Add dialog component and update info card

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -3,6 +3,16 @@ import Hero from "@/components/layout/Hero";
 import { motion } from "framer-motion";
 import InfoCard from "@/components/ui/info-card";
 import Map from "@/components/layout/Map";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import { useTranslations } from "next-intl";
 
 const container = {
   hidden: { opacity: 1, scale: 0 },
@@ -16,6 +26,13 @@ const container = {
   },
 };
 
+const item = {
+  hidden: { y: 40, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 export default function Home() {
   return (
     <div className="flex  flex-col items-center justify-between ">
@@ -27,46 +44,73 @@ export default function Home() {
 }
 
 const Steps = () => {
+  const [selectedStep, setSelectedStep] = useState<number | undefined>();
+  const t = useTranslations("steps");
   return (
     <div className="flex flex-col items-center gap-10 py-20 text-center  bg-accent w-full ">
-      <h4 className="text-xl font-bold">How it works?</h4>
-      <h2 className="text-4xl font-bold">GET IT DONE IN 4 STEPS</h2>
+      <Dialog>
+        <h4 className="text-xl font-bold">How it works?</h4>
+        <h2 className="text-4xl font-bold">GET IT DONE IN 4 STEPS</h2>
 
-      <motion.ul
-        variants={container}
-        whileInView="visible"
-        viewport={{ once: true }}
-        initial="hidden"
-        className="grid grid-cols-4 gap-10 max-w-7xl"
-      >
-        <InfoCard
-          key={1}
-          title="STEP 1"
-          subtitle="Pickup"
-          imageSrc="/images/Address-bro1.png"
-        />
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{t(`step${selectedStep}-details-title`)}</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
 
-        <InfoCard
-          key={2}
-          title="STEP 2"
-          subtitle="Cleaning"
-          imageSrc="https://fakeimg.pl/160x220/ffffff/?text=Step"
-        />
-
-        <InfoCard
-          key={3}
-          title="STEP 3"
-          subtitle="Delivery"
-          imageSrc="https://fakeimg.pl/160x220/ffffff/?text=Step"
-        />
-
-        <InfoCard
-          key={4}
-          title="STEP 4"
-          subtitle="Payment"
-          imageSrc="https://fakeimg.pl/160x220/ffffff/?text=Step"
-        />
-      </motion.ul>
+        <motion.ul
+          variants={container}
+          whileInView="visible"
+          viewport={{ once: true }}
+          initial="hidden"
+          className="grid grid-cols-4 gap-10 max-w-7xl"
+        >
+          <motion.li variants={item}>
+            <DialogTrigger onClick={() => setSelectedStep(1)}>
+              <InfoCard
+                key={1}
+                title="STEP 1"
+                subtitle="Pickup"
+                imageSrc="https://fakeimg.pl/160x220/ffffff/?text=Step"
+              />
+            </DialogTrigger>
+          </motion.li>
+          <motion.li variants={item}>
+            <DialogTrigger onClick={() => setSelectedStep(2)}>
+              <InfoCard
+                key={2}
+                title="STEP 2"
+                subtitle="Cleaning"
+                imageSrc="https://fakeimg.pl/160x220/ffffff/?text=Step"
+              />
+            </DialogTrigger>
+          </motion.li>
+          <motion.li variants={item}>
+            <DialogTrigger onClick={() => setSelectedStep(3)}>
+              <InfoCard
+                key={3}
+                title="STEP 3"
+                subtitle="Delivery"
+                imageSrc="https://fakeimg.pl/160x220/ffffff/?text=Step"
+              />
+            </DialogTrigger>
+          </motion.li>
+          <motion.li variants={item}>
+            <DialogTrigger onClick={() => setSelectedStep(4)}>
+              <InfoCard
+                key={4}
+                title="STEP 4"
+                subtitle="Payment"
+                imageSrc="https://fakeimg.pl/160x220/ffffff/?text=Step"
+              />
+            </DialogTrigger>
+          </motion.li>
+        </motion.ul>
+      </Dialog>
     </div>
   );
 };
--- src/components/ui/info-card.tsx
+++ src/components/ui/info-card.tsx
@@ -1,34 +1,32 @@
-import { motion } from "framer-motion";
+import { Expand } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+
 interface InfoCardProps {
   title: string;
   subtitle: string;
   imageSrc: string;
 }
 
-const item = {
-  hidden: { y: 40, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-  },
-};
 const InfoCard = ({ title, subtitle, imageSrc }: InfoCardProps) => {
   return (
-    <motion.li
-      variants={item}
-      className="w-72 bg-lime/60  rounded-3xl  shadow-xl"
-    >
-      <div className="p-4 flex flex-col gap-4">
-        <h4 className="text-xl">{title}</h4>
-        <h2 className="text-2xl text-primary">{subtitle}</h2>
-      </div>
+    <>
+      <div className="w-72 bg-lime/60  rounded-3xl  shadow-xl  cursor-pointer">
+        <div className="p-4 flex flex-col gap-4">
+          <h4 className="text-xl">{title}</h4>
+          <h2 className="text-2xl text-primary">{subtitle}</h2>
+        </div>
 
-      <div className="flex items-center justify-center p-4">
-        <Image width={160} height={220} src={imageSrc} alt="" />
+        <div className="flex items-center justify-center p-4">
+          <Image width={160} height={220} src={imageSrc} alt="" />
+        </div>
+        {/* more info text */}
+        <div className="flex items-center justify-center p-4">
+          <p className="text-sm text-primary">More info</p>
+          <Expand size={16} className="ml-2 text-secondary" />
+        </div>
       </div>
-    </motion.li>
+    </>
   );
 };
 
